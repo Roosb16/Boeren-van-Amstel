365b0d83c56ce3c12f965ec6c6c237ba751a4527
EvaPols <eva.pols@hva.nl>
2026-06-15 12:59:26 +0200

    foto's toegevoegd

--- public/js/kaart.js
+++ public/js/kaart.js
@@ -89,7 +89,7 @@ const locaties = [
     producten: ["Kaas", "Melk"],
     coords: [52.270, 4.915],
     beschrijving: "Op De Grazige Weide zorgt Vincent Post samen met zijn familie voor zo'n 60 melkkoeien. De boerderij staat voor duurzame landbouw, met aandacht voor biodiversiteit, gezonde graslanden en een goede balans tussen natuur en voedselproductie.",
-    fotos: [],
+    fotos: ["/img/boerderij-grazige-weide.png", "/img/boerderij-grazige-weide2.jpg"],
     quiz: {
       vraag: "Waarom zijn kruidenrijke graslanden belangrijk op De Grazige Weide?",
       antwoorden: [
@@ -109,7 +109,7 @@ const locaties = [
     producten: ["Kaas", "Melk"],
     coords: [52.313, 4.968],
     beschrijving: "Bas en Gerard Timmer geloven dat natuur en landbouw elkaar versterken. Op hun boerderij werken zij aan biodiversiteit, gezonde graslanden en een leefgebied voor weidevogels. Zo produceren ze voedsel met aandacht voor de natuur om hen heen.",
-    fotos: [],
+    fotos: ["/img/boerderij-vredebest.jpg", "/img/boerderij-vredebest2.jpg"],
     quiz: {
       vraag: "Waarom vinden Bas en Gerard de 'korte keten' zo belangrijk?",
       antwoorden: [
@@ -129,7 +129,7 @@ const locaties = [
     producten: [],
     coords: [52.316, 4.960],
     beschrijving: "Beklim Het Poldernest en geniet van een prachtig uitzicht over de Ronde Hoep. Vanaf de 18 meter hoge toren zie je het karakteristieke polderlandschap, de vele sloten en met een beetje geluk zelfs weidevogels in de verte.",
-    fotos: [],
+    fotos: ["/img/uitkijktoren.jpg", "/img/uitkijktoren2.jpg"],
     quiz: {
       vraag: "Waarom heet deze uitkijktoren Het Poldernest?",
       antwoorden: [
@@ -149,7 +149,7 @@ const locaties = [
     producten: [],
     coords: [52.278, 4.908],
     beschrijving: "De Sint-Urbanuskerk is een van de bekendste herkenningspunten van het Amstelland. Met haar opvallende toren steekt de kerk al meer dan 130 jaar boven het polderlandschap uit. Vanaf de Amstel is de kerk van verre te zien en vormt zij een bijzonder stukje geschiedenis midden in de natuur.",
-    fotos: [],
+    fotos: ["/img/kerk-urbanus.jpg", "/img/kerk-urbanus2.jpg"],
     quiz: {
       vraag: "Hoeveel Urbanuskerken zijn er in het Amstelland?",
       antwoorden: [
@@ -169,7 +169,7 @@ const locaties = [
     producten: [],
     coords: [52.290, 4.920],
     beschrijving: "De pontjes van het Amstelland verbinden fiets- en wandelroutes aan beide kanten van de Amstel. Al jarenlang helpen ze bezoekers om het polderlandschap op een bijzondere manier te ontdekken, vanaf het water én vanaf de dijken.",
-    fotos: [],
+    fotos: ["/img/pontveer.jpg", "/img/pontveer2.png"],
     quiz: {
       vraag: "Hoe steken de meeste pontjes in het Amstelland de rivier over?",
       antwoorden: [
@@ -189,7 +189,7 @@ const locaties = [
     producten: ["Kaas"],
     coords: [52.303, 4.976],
     beschrijving: "Restaurant met streekproducten uit het Amstelgebied.",
-    fotos: [],
+    fotos: ["/img/restaurant-voetangel.jpg", "/img/restaurant-voetangel2.png"],
     quiz: {
       vraag: "Wat serveert De Voetangel?",
       antwoorden: [
@@ -209,7 +209,7 @@ const locaties = [
     producten: [],
     coords: [52.298, 4.952],
     beschrijving: "De weidevogels worden beschermd en hier kunnen ze veilig broeden.",
-    fotos: [],
+    fotos: ["/img/grutto-nesten.jpg", "/img/grutto-nesten2.jpg"],
     quiz: {
       vraag: "Waarom is dit gebied belangrijk?",
       antwoorden: [
